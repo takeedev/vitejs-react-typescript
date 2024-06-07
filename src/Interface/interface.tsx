ed7c46cb084fe5a33ec2c61f07c02f549f979d1a
taknaja <peerapolkoeram@gmail.com>
2024-06-07 21:12:24 +0700

    add interface and page index

---
+++ src/Interface/interface.tsx
@@ -0,0 +1,12 @@
+export default interface CardTypeInterface {
+    id: Number
+    , name: String
+    , typeNum: String
+}
+
+export default interface OliComTypeInterface {
+    id: Number
+    , name: String
+    , oilNum: String
+    , typeId: String
+}
---
+++ src/page/index.tsx
@@ -0,0 +1,9 @@
+import AddressPage from "./address/addressPage";
+import Home from "./home/home";
+import DropdownPage from "./dropdown/dropdownPage";
+
+export default {
+    AddressPage
+    ,Home
+    ,DropdownPage
+}
