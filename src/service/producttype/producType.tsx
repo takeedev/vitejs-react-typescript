806605211003d0a279065a4b8cbddce21d68662a
taknaja <peerapolkoeram@gmail.com>
2024-06-08 22:52:04 +0700

    add product

---
+++ src/service/producttype/producType.tsx
@@ -0,0 +1,14 @@
+const ProducType = [
+    {
+        "id": 1
+        , "name": "Computer"
+        , "subject": "1"
+    },
+    {
+        "id": 2
+        , "name": "Keyboard"
+        , "subject": "2"
+    }
+]
+
+export default ProducType
---
+++ src/service/producttype/productMain.tsx
@@ -0,0 +1,28 @@
+const ProducMain = [
+    {
+        "id": 1
+        , "name": "Computer ax"
+        , "number": "1000"
+        , "typeId": "1"
+    },
+    {
+        "id": 2
+        , "name": "Keybord ad"
+        , "number": "1001"
+        , "typeId": "1"
+    },
+    {
+        "id": 3
+        , "name": "Computer ax"
+        , "number": "2000"
+        , "typeId": "2"
+    },
+    {
+        "id": 4
+        , "name": "Keybord ad"
+        , "number": "2001"
+        , "typeId": "2"
+    }
+]
+
+export default ProducMain
