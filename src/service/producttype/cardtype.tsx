349de3f80dfbad3e3ffc546d61ab45f916272042
taknaja <peerapolkoeram@gmail.com>
2024-06-09 21:51:38 +0700

    add service product and producttype

---
+++ src/service/producttype/cardtype.tsx
@@ -0,0 +1,36 @@
+const cardType = [
+  {
+    "id": 1
+    , "name": "normal"
+    , "typeNum": "100"
+    , "oilcom":
+      [{
+        "id": 1
+        , "name": "PTT"
+        , "typeNum": "001"
+      }
+        , {
+        "id": 2
+        , "name": "SHELL"
+        , "typeNum": "100"
+      }]
+  },
+  {
+    "id": 2
+    , "name": "VIP"
+    , "typeNum": "200"
+    , "oilcom":
+      [{
+        "id": 1
+        , "name": "PTT"
+        , "typeNum": "201"
+      }
+        , {
+        "id": 2
+        , "name": "SHELL"
+        , "typeNum": "202"
+      }]
+  }
+]
+
+export default cardType
---
+++ src/service/producttype/product.tsx
@@ -0,0 +1,14 @@
+const Product = [
+    {
+        "id": 1
+        , "name": "Notbook Acer"
+        , "subject": "01"
+    },
+    {
+        "id": 2
+        , "name": "Keyboard Keykron"
+        , "subject": "02"
+    }
+]
+
+export default Product
