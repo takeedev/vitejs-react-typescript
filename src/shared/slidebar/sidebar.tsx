39d48b295784f39f56c59c6cd3714440924a30b9
taknaja <peerapolkoeram@gmail.com>
2024-12-27 21:42:34 +0700

    update sidebar

--- src/shared/slidebar/sidebar.tsx
+++ src/shared/slidebar/sidebar.tsx
@@ -1,89 +1,93 @@
-// @ts-ignore
 import logo from '../../assets/logo-tpk-v2.png';
 import menu from '../../service/menu/menu';
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {NavLink} from 'react-router-dom';
 import './sidebar.css'
 
-const Sidebar = () => {
-    const [openMenus, setOpenMenus] = useState<{ [key: string]: boolean }>({});
-    const [openSubMenus, setOpenSubMenus] = useState<{ [key: string]: boolean }>({});
+interface SidebarProps {
+    setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    isSidebarOpen: boolean;
+    toggleSidebar: () => void;
+}
 
-    const toggleMenu = (menuName: string): void => {
-        setOpenMenus((prev) => ({
+const Sidebar: React.FC<SidebarProps> = ({setIsSidebarOpen, isSidebarOpen, toggleSidebar}) => {
+
+    const [menuStates, setMenuStates] = useState<{
+        [menuName: string]: { isOpen: boolean; subMenu?: { [subMenuName: string]: boolean } };
+    }>({});
+    const toggleMenu = (menuName: string) => {
+        setMenuStates((prev) => ({
             ...prev,
-            [menuName]: !prev[menuName],
+            [menuName]: {
+                ...prev[menuName],
+                isOpen: !prev[menuName]?.isOpen,
+            },
         }));
     };
 
-    const toggleSubMenu = (menuName: string): void => {
-        setOpenSubMenus((prev) => ({
+    const toggleSubMenu = (menuName: string, subMenuName: string) => {
+        setMenuStates((prev) => ({
             ...prev,
-            [menuName]: !prev[menuName],
+            [menuName]: {
+                ...prev[menuName],
+                subMenu: {
+                    ...prev[menuName]?.subMenu,
+                    [subMenuName]: !prev[menuName]?.subMenu?.[subMenuName],
+                },
+            },
         }));
     };
 
     return (
         <>
-            <div className="container-slidbar">
-                <div className="nav-header">
+            <div className={`container-sidebar ${isSidebarOpen ? "open" : "closed"}`}>
+                <div className={`nav-header`}>
+                    <i className={"ti ti ti-caret-left-filled"} onClick={toggleSidebar}></i>
                     <div className="logo-image">
                         <img src={logo} alt="Logo"/>
                     </div>
                 </div>
                 {/* link menu */}
-                <div className="nav-link">
-                    {menu.map((menus, index) => (
-                        <li key={index}>
-                            {menus.sub?.length == null ? (
-                                <div>
-                                    <a href="#">
-                                        <i className={menus.icon}></i>
-                                        <span className="link-name-topic">{menus.name}</span>
-                                    </a>
-                                </div>
-                            ) : (
-                                <div className="main-menu" onClick={() => toggleMenu(menus.name)}>
-                                    <a href="#">
-                                        <i className={menus.icon}></i>
-                                        <span className="link-name-topic">{menus.name}</span>
-                                    </a>
-                                </div>
-                            )}
-                            {/* submenu */}
-                            {menus.sub?.length == null ? "" : (
-                                <ul className={`sub-menu${openMenus[menus.name] ? " show-menu" : ""}`}>
-                                    {menus.sub?.map((sub, index) => (
-                                        <li key={index}>
-                                            <div onClick={() => toggleSubMenu(menus.name)}>
-                                                <a href="#" className="link-name">
-                                                    <i className="ti ti-point-filled"></i>
-                                                    {sub.name}
-                                                </a>
+                <div className="menu">
+                    {menu.map((menus) => (
+                        <div key={menus.name}>
+                            <div className="menu-item" onClick={() => toggleMenu(menus.name)}>
+                                <i className={menus.icon}></i>
+                                <span>{menus.name}</span>
+                            </div>
+                            {menus.sub && (
+                                <div className={`submenu ${menuStates[menus.name]?.isOpen ? "show" : ""}`}>
+                                    {menus.sub.map((sub) => (
+                                        <div key={sub.name} className="submenu-item">
+                                            <div onClick={() => toggleSubMenu(menus.name, sub.name)}>
+                                                <span>- {sub.name}</span>
                                             </div>
-                                            {sub.sub?.length === null ? "" : (
-                                                <ul className={`link-subs${openSubMenus[menus.name] ? " show-menu" : ""}`}>
-                                                    {/* sub menu 2 */}
-                                                    {sub.sub?.map((sub, index) => (
-                                                        <div key={index}>
-                                                            <NavLink to={sub.link}>
-                                                                <a>
-                                                                    <i className="ti ti-point-filled"></i>
-                                                                    {sub.name}
-                                                                </a>
+                                            {sub.sub && (
+                                                <div
+                                                    className={`sub-submenu ${menuStates[menus.name]?.subMenu?.[sub.name] ? "show" : ""}`}>
+                                                    {sub.sub.map((sub2) => (
+                                                        <div key={sub2.name} className="sub-submenu-end">
+                                                            <NavLink to={sub2.link}
+                                                                     className={({isActive}) => (isActive ? "active-link" : "inactive-link")}>
+                                                                <div key={sub2.name}>
+                                                                    {sub2.name}
+                                                                </div>
                                                             </NavLink>
                                                         </div>
                                                     ))}
-                                                </ul>
+                                                </div>
                                             )}
-                                        </li>
+                                        </div>
                                     ))}
-                                </ul>
+                                </div>
                             )}
-                        </li>
+                        </div>
                     ))}
                 </div>
             </div>
+            {!isSidebarOpen && (
+                <i className={"ti ti ti-caret-right-filled open-icon"} onClick={() => setIsSidebarOpen(true)}></i>
+            )}
         </>
     )
 }
